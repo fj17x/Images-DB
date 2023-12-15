069179ca9d64dc43501311bf60b242c60b862fea
Firas <awesomefirasjavid@gmail.com>
2023-12-15 13:55:23 +0530

    Added updating image data. Also added REST methods.

--- routes/image.js
+++ routes/image.js
@@ -4,8 +4,10 @@ import Image from "../models/image.js"
 import verifyToken from "../middleware/tokenAuth.js"
 
 const userRouter = express.Router()
+const imagesFilePath = "db/images.json"
 
-userRouter.post("/uploadImage", verifyToken, async (req, res) => {
+// POST method to upload an image URL.
+userRouter.post("/images", verifyToken, async (req, res) => {
   try {
     //Get metadata provided in JSON and JWT.
     let { imageURL, title, description, tags } = req.body
@@ -24,18 +26,18 @@ userRouter.post("/uploadImage", verifyToken, async (req, res) => {
     }
 
     //Get username of user using given ID.
-    const allUsersJSON = await fs.readFile("db/users.json", "utf-8")
+    const allUsersJSON = await fs.readFile(imagesFilePath, "utf-8")
     const allUsersObject = JSON.parse(allUsersJSON)
     const user = allUsersObject.users.find((user) => user.id === userId)
     if (!user) {
-      return res.status(400).json({
+      return res.status(404).json({
         error: "User doesnt exist.",
       })
     }
     const userName = user.username
 
     //Get current Image DB as an object.
-    const allImagesJSON = await fs.readFile("db/images.json", "utf-8")
+    const allImagesJSON = await fs.readFile(imagesFilePath, "utf-8")
     const allImagesObject = JSON.parse(allImagesJSON)
 
     //Create new Image object.
@@ -45,30 +47,34 @@ userRouter.post("/uploadImage", verifyToken, async (req, res) => {
     //Push image into images object and write on JSON DB.
     allImagesObject.images.push(newImageObject)
     await fs.writeFile("db/images.json", JSON.stringify(allImagesObject, null, 2))
-    res.status(200).json({ message: `Sucessfully uploaded image! Image Id is : ${newImageId}` })
+    res.status(201).json({ message: `Sucessfully uploaded image! Image Id is : ${newImageId}` })
   } catch (err) {
-    console.log("Error while uploading", err)
+    console.log("Error during uploading: ", err)
     res.status(500).json({ error: "Failed to upload. Must provide url and title." })
   }
 })
 
-userRouter.post("/fetchImageById", verifyToken, async (req, res) => {
+// GET method to get fetch an image using ID.
+userRouter.get("/images/:imageId", verifyToken, async (req, res) => {
   try {
     //Get ID of image needed to be searched for.
-    const { imageId } = req.body
+    const { imageId } = req.params
     const userId = req.userId
     if (!imageId) {
       return res.status(400).json({ error: "Please provide imageId." })
     }
 
-    //Get the JSON DB for images as an object.
-    const allImagesJSON = await fs.readFile("db/images.json", "utf-8")
+    //Get current JSON DB images as an object.
+    const allImagesJSON = await fs.readFile(imagesFilePath, "utf-8")
     const allImagesObject = JSON.parse(allImagesJSON)
 
     //Search for the image and check whether user has permission to access it.
     const foundImage = allImagesObject.images.find((image) => image.id === imageId)
     if (!foundImage) {
       return res.status(404).json({ error: "Image not found." })
+    }
+    if (foundImage.tagged) {
+      return res.status(400).json({ error: "This image has been tagged by the admin and cannot be accessed." })
     }
     if (foundImage.ownerId !== userId) {
       return res.status(401).json({ error: "You can only access images you have uploaded." })
@@ -79,6 +85,123 @@ userRouter.post("/fetchImageById", verifyToken, async (req, res) => {
   } catch (err) {
     console.log("Error while fetching.", err)
     res.status(500).json({ error: "Failed to fetch." })
+  }
+})
+
+// GET method to fetch batch of images.
+userRouter.get("/images", verifyToken, async (req, res) => {
+  try {
+    //Get limit and offset and calculate start and end index.
+    let { limit, offset } = req.params
+    if (!limit) {
+      limit = 10
+    }
+    if (!offset) {
+      offset = 0
+    }
+
+    //Get current JSON DB images as an object.
+    const allImagesJSON = await fs.readFile(imagesFilePath, "utf-8")
+    const allImagesObject = JSON.parse(allImagesJSON)
+
+    //Calculate start and end
+    const startIndex = offset
+    const endIndex = startIndex + limit
+
+    //Filter images to get user's images and non-tagged images.
+    const userImages = allImagesObject.images.filter((image) => {
+      return !image.tagged && image.userId === userId
+    })
+
+    //Get batch of images and send back to user.
+    const batchOfUserImages = userImages.slice(startIndex, endIndex)
+    if (!batchOfUserImages.length) {
+      return res.status(404).json({ error: "No images found." })
+    }
+    res.status(200).json({ images: batchOfUserImages })
+  } catch (err) {
+    console.error("Error during fetching: ", err)
+    res.status(500).json({ error: "Failed to fetch images." })
+  }
+})
+
+// PUT method to update the description of an image.
+userRouter.put("/images/:imageId/description", verifyToken, async (req, res) => {
+  try {
+    //Get the ID of the image to update along with description.
+    const { imageId, description } = req.body
+    const userId = req.userId
+    if (!imageId) {
+      return res.status(400).json({ error: "Please provide imageId." })
+    }
+    if (!description) {
+      return res.status(400).json({ error: "Please provide description." })
+    }
+
+    //Fetch all the images from JSON DB as JS object.
+    const allImagesJSON = await readFile(imagesFilePath, "utf-8")
+    const allImagesObject = JSON.parse(allImagesJSON)
+
+    //Find image and verify whether user has permissions to modify it.
+    const foundImage = allImagesObject.images.find((image) => image.id === imageId)
+    if (!foundImage) {
+      return res.status(404).json({ error: "Please provide a valid imageId." })
+    }
+    if (foundImage.tagged) {
+      return res.status(400).json({ error: "This image has been tagged by the admin and cannot be accessed." })
+    }
+    if (foundImage.ownerId !== userId) {
+      return res.status(401).json({ error: "You can only modify images you have uploaded." })
+    }
+
+    //Update description and update JSON DB. (Will update as objects are referenced by memory address.)
+    foundImage.description = description
+    await fs.writeFile(imagesFilePath, JSON.stringify(allImagesObject, null, 2))
+    res.status(200).json({ message: "Image description updated successfully." })
+  } catch (err) {
+    console.error("Error during updating: ", err)
+    res.status(500).json({ error: "Failed to update image description." })
+  }
+})
+
+// PUT method to update the tags of an image.
+userRouter.put("/images/:imageId/tags", verifyToken, async (req, res) => {
+  try {
+    const { imageId, tags } = req.body
+    const userId = req.userId
+    if (!imageId) {
+      return res.status(400).json({ error: "Please provide imageId." })
+    }
+    if (!tags) {
+      return res.status(400).json({ error: "Please provide description." })
+    }
+    if (!Array.isArray(tags)) {
+      return res.status(400).json({ error: "Please provide tags as an array." })
+    }
+
+    //Get current JSON DB images as an object.
+    const allImagesJSON = await fs.readFile(imagesFilePath, "utf-8")
+    const allImagesObject = JSON.parse(allImagesJSON)
+
+    //Find image and verify whether user has permissions to modify it.
+    const foundImage = allImagesObject.images.find((image) => image.id === imageId)
+    if (!foundImage) {
+      return res.status(404).json({ error: "Image not found." })
+    }
+    if (foundImage.tagged) {
+      return res.status(400).json({ error: "This image has been tagged by the admin and cannot be accessed." })
+    }
+    if (foundImage.userId !== userId) {
+      return res.status(403).json({ error: "Unauthorized to update tags for this image." })
+    }
+
+    //Update the tags in the image and update JSON DB.
+    foundImage.tags.push(...tags)
+    await fs.writeFile(imagesFilePath, JSON.stringify(imageData, null, 2))
+    res.status(200).json({ message: "Tags added successfully." })
+  } catch (err) {
+    console.error("Error during adding tags: ", err)
+    res.status(500).json({ error: "Failed to add tags to image." })
   }
 })
 
--- routes/user.js
+++ routes/user.js
@@ -6,88 +6,75 @@ import "dotenv/config"
 import User from "../models/user.js"
 
 const userRouter = express.Router()
+const usersFilePath = "db/users.json"
 
-userRouter.post("/register", async (req, res) => {
+// POST method to register a new user.
+userRouter.post("/users", async (req, res) => {
   try {
-    //Get secret key,username and hashed password.
     const secretKey = process.env.JWT_SECRET_KEY ?? "THISISFUN"
     const { userName, password } = req.body
-    if (!userName) {
-      return res.status(400).json({ error: "Please provide the username." })
-    }
-    if (!password) {
-      return res.status(400).json({ error: "Please provide the password." })
-    }
-    const passwordToString = password.toString()
 
+    if (!userName || !password) {
+      return res.status(400).json({ error: "Please provide username and password." })
+    }
+
+    const passwordToString = password.toString()
     const saltRounds = 15
     const hashedPassword = await bcrypt.hash(passwordToString, saltRounds)
 
-    //Get current JSON DB as an object.
-    const allUsersJSON = await fs.readFile("db/users.json", "utf-8")
+    const allUsersJSON = await fs.readFile(usersFilePath, "utf-8")
     const allUsersObject = JSON.parse(allUsersJSON)
 
-    //Check whether username already exists.
     const existingUser = allUsersObject.users.find((user) => user.username === userName)
     if (existingUser) {
       return res.status(400).json({ error: "Username already exists. Choose a different username." })
     }
 
-    //Create new User object.
     const newUserId = (allUsersObject.users?.length ?? 0) + 1
     const newUserObject = new User(newUserId, userName, hashedPassword)
 
-    //Push user into users object and write on the JSON DB.
     allUsersObject.users.push(newUserObject)
-    await fs.writeFile("db/users.json", JSON.stringify(allUsersObject, null, 2))
+    await fs.writeFile(usersFilePath, JSON.stringify(allUsersObject, null, 2))
 
-    //Sign with secret key and send token as json.
     const jwtToken = jwt.sign({ userId: newUserId }, secretKey)
     console.log(`A new user has registered with ID = ${newUserId} & username = '${userName}'`)
-    res.status(200).json({ jwtToken, message: "Sucessfully registered!" })
+    res.status(201).json({ jwtToken, message: "Successfully registered!" })
   } catch (err) {
-    console.log("Error while registering", err)
+    console.log("Error during registering: ", err)
     res.status(500).json({ error: "Failed to register." })
   }
 })
 
-userRouter.post("/login", async (req, res) => {
+// POST method to log in.
+userRouter.post("/users/login", async (req, res) => {
   try {
-    //Get secret key and user Id.
     const secretKey = process.env.JWT_SECRET_KEY ?? "THISISFUN"
     const { userName, password } = req.body
+
+    if (!userName || !password) {
+      return res.status(400).json({ error: "Please provide username and password." })
+    }
+
     const passwordToString = password.toString()
-
-    if (!userName) {
-      return res.status(400).json({ error: "Please provide the username." })
-    }
-    if (!password) {
-      return res.status(400).json({ error: "Please provide the password." })
-    }
-
-    //Get current JSON DB.
-    const allUsersJSON = await fs.readFile("db/users.json", "utf-8")
+    const allUsersJSON = await fs.readFile(usersFilePath, "utf-8")
     const allUsersObject = JSON.parse(allUsersJSON)
 
-    //Check if the user exists and get their ID.
     const user = allUsersObject.users.find((user) => user.username === userName)
     if (!user) {
       return res.status(400).json({ error: "Such a user does not exist. Please register first." })
     }
 
-    //Check whether password matches.
     const passwordMatches = await bcrypt.compare(passwordToString, user.password)
     if (!passwordMatches) {
       return res.status(401).json({ error: "Your password is incorrect." })
     }
 
-    //Sign with secret key and send token as json.
     const userId = user.id
     const jwtToken = jwt.sign({ userId }, secretKey)
     console.log(`ID ${userId} requested for their token!`)
-    res.status(200).json({ jwtToken, message: "Sucessfully logged in!" })
+    res.status(200).json({ jwtToken, message: "Successfully logged in!" })
   } catch (err) {
-    console.log("Error while logging in.", err)
+    console.log("Error during logging in: ", err)
     res.status(500).json({ error: "Failed to login." })
   }
 })
