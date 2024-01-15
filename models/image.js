7467688cfcfad3db18a0e19d8845c9a50e4fe4b6
Firas <awesomefirasjavid@gmail.com>
2024-01-15 13:40:46 +0530

    Added sequelize models i.e User and Images.

--- models/image.js
+++ models/image.js
@@ -1,15 +1,75 @@
-export default class Image {
-  isFlagged = false
-  isDeleted = false
-  constructor(id, url, title, description = "", ownerId, ownerUserName, tags = []) {
-    this.id = id
-    this.url = url
-    this.title = title
-    this.description = description
-    this.createdAt = new Date()
-    this.modifiedAt = new Date()
-    this.ownerId = ownerId
-    this.ownerUserName = ownerUserName
-    this.tags = tags
+import { DataTypes } from "sequelize"
+import sequelize from "../db/connection.js"
+
+const Image = sequelize.define(
+  "User",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      allowNull: false,
+      autoIncrement: true,
+      unique: true,
+    },
+    url: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.STRING,
+      defaultValue: "",
+    },
+    // createdAt: {
+    //   type: DataTypes.DATE,
+    //   defaultValue: DataTypes.NOW,
+    // },
+    // modifiedAt: {
+    //   type: DataTypes.DATE,
+    //   defaultValue: DataTypes.NOW,
+    // },
+    ownerId: {
+      type: DataTypes.INTEGER,
+    },
+    ownerUserName: {
+      type: DataTypes.STRING,
+    },
+    tags: {
+      type: DataTypes.ARRAY(DataTypes.STRING),
+      defaultValue: [],
+    },
+    isFlagged: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
+    },
+    // isDeleted: {
+    //   type: DataTypes.BOOLEAN,
+    //   defaultValue: false,
+    // },
+  },
+  {
+    paranoid: true,
+    deletedAt: "destroyTime",
   }
-}
+)
+
+export default Image
+
+// export default class Image {
+//   isFlagged = false
+//   isDeleted = false
+//   constructor(id, url, title, description = "", ownerId, ownerUserName, tags = []) {
+//     this.id = id
+//     this.url = url
+//     this.title = title
+//     this.description = description
+//     this.createdAt = new Date()
+//     this.modifiedAt = new Date()
+//     this.ownerId = ownerId
+//     this.ownerUserName = ownerUserName
+//     this.tags = tags
+//   }
+// }
--- models/user.js
+++ models/user.js
@@ -1,11 +1,64 @@
-export default class User {
-  isAdmin = false
-  isDeleted = false
-  constructor(id, userName, password) {
-    this.id = id
-    this.userName = userName
-    this.password = password
-    this.createdAt = new Date()
-    this.modifiedAt = new Date()
+import { DataTypes } from "sequelize"
+import sequelize from "../db/connection.js"
+
+const User = sequelize.define(
+  "User",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      allowNull: false,
+      autoIncrement: true,
+      unique: true,
+    },
+    userName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    // createdAt: {
+    //   type: DataTypes.DATE,
+    //   defaultValue: DataTypes.NOW,
+    // },
+    // modifiedAt: {
+    //   type: DataTypes.DATE,
+    //   defaultValue: DataTypes.NOW,
+    // },
+    ownerUserName: {
+      type: DataTypes.STRING,
+    },
+    tags: {
+      type: DataTypes.ARRAY(DataTypes.STRING),
+      defaultValue: [],
+    },
+    // isDeleted: {
+    //   type: DataTypes.BOOLEAN,
+    //   defaultValue: false,
+    // },
+    isAdmin: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
+    },
+  },
+  {
+    paranoid: true,
+    deletedAt: "destroyTime",
   }
-}
+)
+
+export default User
+
+// export default class User {
+//   isAdmin = false
+//   isDeleted = false
+//   constructor(id, userName, password) {
+//     this.id = id
+//     this.userName = userName
+//     this.password = password
+//     this.createdAt = new Date()
+//     this.modifiedAt = new Date()
+//   }
+// }
