443beaabb85fc1f75454c1ab390a8de7d253e07a
Firas <awesomefirasjavid@gmail.com>
2024-01-19 14:32:47 +0530

    Removed manual binding of connection URI

--- src/config/connection.js
+++ src/config/connection.js
@@ -1,18 +1,13 @@
 import { Sequelize } from "sequelize"
 import "dotenv/config"
 
-const connectionURI =
-  "postgres://" +
-  process.env.DB_USERNAME +
-  ":" +
-  process.env.DB_PASSWORD +
-  "@" +
-  process.env.DB_HOST +
-  ":" +
-  process.env.DB_PORT +
-  "/" +
-  process.env.DB_NAME
-
-const sequelize = new Sequelize(connectionURI)
+const sequelize = new Sequelize({
+  dialect: "postgres",
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+})
 
 export default sequelize
