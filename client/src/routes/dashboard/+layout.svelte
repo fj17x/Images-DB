094a9240c888a6bcb34f29202c8ca696d9b5477b
Firas <awesomefirasjavid@gmail.com>
2024-02-02 21:48:58 +0530

    Added check for entering dashboard.

---
+++ client/src/routes/dashboard/+layout.svelte
@@ -0,0 +1,20 @@
+<script>
+  import { goto } from "$app/navigation"
+  import { onMount } from "svelte"
+
+  const checkSignedIn = async () => {
+    const response = await fetch(`http://localhost:4000/images`, {
+      method: "GET",
+      credentials: "include",
+    })
+    if (!response.ok) {
+      goto("/")
+    }
+  }
+
+  onMount(async () => {
+    await checkSignedIn()
+  })
+</script>
+
+<slot />
