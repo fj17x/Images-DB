95061b81b434b83803dc16feda1db72847a6b722
Firas <awesomefirasjavid@gmail.com>
2024-02-03 14:39:09 +0530

    Add validation for signin

--- client/src/routes/dashboard/+layout.svelte
+++ client/src/routes/dashboard/+layout.svelte
@@ -1,6 +1,10 @@
 <script>
+  import AlertModal from "$lib/components/AlertModal.svelte"
   import { goto } from "$app/navigation"
   import { onMount } from "svelte"
+
+  let showAlertModal = false
+  let alertModalOptions = {}
 
   const checkSignedIn = async () => {
     const response = await fetch(`http://localhost:4000/me`, {
@@ -8,13 +12,26 @@
       credentials: "include",
     })
     if (!response.ok) {
-      goto("/")
+      {
+        alertModalOptions.header = "Cannot access page"
+        alertModalOptions.message = `Please sign in.`
+        alertModalOptions.type = "failure"
+        showAlertModal = true
+      }
     }
+  }
+  const onConfirm = () => {
+    goto("/")
+    showAlertModal = false
   }
 
   onMount(async () => {
     await checkSignedIn()
   })
 </script>
+
+{#if showAlertModal}
+  <AlertModal bind:showModal={showAlertModal} {...alertModalOptions} {onConfirm}></AlertModal>
+{/if}
 
 <slot />
