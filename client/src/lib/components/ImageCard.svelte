b6a9d5da828d8c22979c83449db1d780e52dd7ae
Firas <awesomefirasjavid@gmail.com>
2024-02-05 12:40:54 +0530

    Added loading spinner to images loading

--- client/src/lib/components/ImageCard.svelte
+++ client/src/lib/components/ImageCard.svelte
@@ -1,15 +1,43 @@
 <script>
+  import { onMount } from "svelte"
   export let url, title, id, destroyTime
+  let isLoading = true
+
+  function handleImageLoad() {
+    isLoading = false
+  }
+
+  onMount(() => {
+    const image = new Image()
+    image.src = url
+    image.onload = handleImageLoad
+  })
 </script>
 
 <div class="card">
   <a href="/dashboard/myimages/{id}" class="anchor">
-    <img src={url} alt={title} />
-    <p class="title {destroyTime ? 'destroyed' : 'normal'}">{title}</p>
+    {#if isLoading}
+      <div class="loading-spinner">
+        <i class="fas fa-spinner fa-spin"></i>
+      </div>
+    {:else}
+      <img src={url} alt={title} on:load={handleImageLoad} />
+      <p class="title {destroyTime ? 'destroyed' : 'normal'}">{title}</p>
+    {/if}
   </a>
 </div>
 
 <style>
+  .loading-spinner {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: black;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 200px;
+  }
   .card {
     border-radius: 8px;
     overflow: hidden;
