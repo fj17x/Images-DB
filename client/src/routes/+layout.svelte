9924809ced2880a7ecd59ffeaf271cef8f4d0c6e
Firas <awesomefirasjavid@gmail.com>
2024-02-05 16:33:06 +0530

    Moved checking sign in to home layout.

--- client/src/routes/+layout.svelte
+++ client/src/routes/+layout.svelte
@@ -1,5 +1,29 @@
 <script>
+  import { userDetails } from "../stores/userDetails.js"
+  import { onMount } from "svelte"
   import "@fortawesome/fontawesome-free/css/all.min.css"
+
+  const checkSignedIn = async () => {
+    const response = await fetch(`http://localhost:4000/me`, {
+      method: "GET",
+      credentials: "include",
+    })
+    if (!response.ok) {
+      {
+        alertModalOptions.header = "Cannot access page"
+        alertModalOptions.message = `Please sign in.`
+        alertModalOptions.type = "failure"
+        showAlertModal = true
+        return
+      }
+    }
+    const reply = await response.json()
+    userDetails.set(reply.data)
+  }
+
+  onMount(async () => {
+    await checkSignedIn()
+  })
 </script>
 
 <slot />
--- client/src/routes/dashboard/+layout.svelte
+++ client/src/routes/dashboard/+layout.svelte
@@ -1,37 +1,14 @@
 <script>
   import AlertModal from "$lib/components/AlertModal.svelte"
   import { goto } from "$app/navigation"
-  import { onMount } from "svelte"
-  import { userDetails } from "../../stores/userDetails.js"
 
   let showAlertModal = false
   let alertModalOptions = {}
 
-  const checkSignedIn = async () => {
-    const response = await fetch(`http://localhost:4000/me`, {
-      method: "GET",
-      credentials: "include",
-    })
-    if (!response.ok) {
-      {
-        alertModalOptions.header = "Cannot access page"
-        alertModalOptions.message = `Please sign in.`
-        alertModalOptions.type = "failure"
-        showAlertModal = true
-        return
-      }
-    }
-    const reply = await response.json()
-    userDetails.set(reply.data)
-  }
   const onAlertConfirm = () => {
     goto("/")
     showAlertModal = false
   }
-
-  onMount(async () => {
-    await checkSignedIn()
-  })
 </script>
 
 {#if showAlertModal}
